481ec9c244586ed38bd4b7e8f8bdbec48671871b
haeram.park <haeram.park@tisquare.com>
2021-06-17 15:15:37 +0900

    innerwidth size 실시간 체크

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+      <div>{{ window.width }}</div>
       <admin-layout></admin-layout>
   </div>
 </template>
@@ -8,10 +9,40 @@
 import AdminLayout from "@/layout/AdminLayout.vue";
 export default {
   name: 'App',
+  data(){
+    return{
+    window:{
+      width: 0
+     },
+     isUnderSize:false // innerwidth 990 이하인지 체크 , 디폴트 : 이하 아님 
+    };
+  },
   components: {
     AdminLayout
+  },
+  created(){
+    window.addEventListener('resize', this.handleResize);
+    this.handleResize();
+  },
+  mounted(){
+   
+  },
+  destoryed(){
+    window.removeEventListener('resize', this.handleResize);
+  },
+  methods:{
+    handleResize(){
+      this.window.width = window.innerWidth;
+      let widthSize = window.innerWidth; 
+      if(widthSize<=990){
+        this.isUnderSize = true; 
+      }else{
+        this.isUnderSize = false; 
+      }
+    },
   }
 }
+
 </script>
 
 <style>
